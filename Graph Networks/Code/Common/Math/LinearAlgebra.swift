ac4966518221d4bc1580a42ecf412aef0fa0324b
Justin Cook <jcook03266@gmail.com>
2024-02-06 11:46:35 -0500

    Added 3D connectors between graph vertices + a simple settings menu

---
+++ Graph Networks/Code/Common/Math/LinearAlgebra.swift
@@ -0,0 +1,8 @@
+//
+//  LinearAlgebra.swift
+//  Graph Networks
+//
+//  Created by Justin Cook on 2/5/24.
+//
+
+import Foundation
---
+++ Graph Networks/Code/Controllers/CameraController.swift
@@ -0,0 +1,8 @@
+//
+//  CameraController.swift
+//  Graph Networks
+//
+//  Created by Justin Cook on 2/1/24.
+//
+
+import Foundation
